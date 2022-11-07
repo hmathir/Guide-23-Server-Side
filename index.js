e445b55600b1f9091c7d63391be30993d1f8c3d6
hmathir <hmathir@gmail.com>
2022-11-07 23:22:22 +0600

    Database Connected And Services API Created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
@@ -10,6 +11,31 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server Running...');
 });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dmmiwed.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//Database Connected
+const dbConnect = async () => {
+    try{
+        await client.connect();
+        console.log('Server Connected');
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+dbConnect();
+
+const serviceCollection = client.db('guide23').collection('services');
+
+app.get('/services', async(req, res) => {
+    const query = req.query.limit || 0;
+    const cursor = serviceCollection.find({});
+    const services = await cursor.limit(parseInt(query)).toArray();
+    res.send(services);
+})
 
 app.listen(port, () => {
     console.log(`App listening at ${port}`);
